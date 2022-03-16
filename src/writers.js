68eccd1ca2e9c53bf54b0ce9b5fdcb2e5b3988fb
Dumba-Theresa03 <dumbatheresa3@gmail.com>
2022-03-16 16:04:03 +0000

    first comit

---
+++ src/writers.js
@@ -0,0 +1,40 @@
+const writers=[
+{
+  id:1,
+  name:"Jamila lorde",
+  avatar:"audre_lorde",
+  email:"jamila23@gmail.com",
+  phone:"0507939300",
+
+},
+{
+  id:2,
+  name:"Dumba Theresa",
+  avatar:"belle_hooks",
+  email:"thess23@gmail.com",
+  phone:"0507939300",
+},
+{
+  id:3,
+  name:"Samad Latifa",
+  avatar:"du_bois",
+  email:"lat23@gmail.com",
+  phone:"0507939300",
+},
+{
+  id:4,
+  name:"Iddrisu Hanifa",
+  avatar:"james_baldwin",
+  email:"hanifa23@gmail.com",
+  phone:"0507939300",
+},
+{
+  id:5,
+  name:"Inusah Razak",
+  avatar:"maya_angelou",
+  email:"inusah23@gmail.com",
+  phone:"0507939300",
+}
+]
+
+export default writers;
